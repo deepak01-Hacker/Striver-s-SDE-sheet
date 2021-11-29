bac271686d1365a2af544019227852cbfb162f64
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-11-29 12:10:05 +0530

    BITS

---
+++ Day 12/#Power of Two.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        
+        while(n>1 and (n&1)==0):
+            n >>= 1
+        
+        return n==1
+        
+        
