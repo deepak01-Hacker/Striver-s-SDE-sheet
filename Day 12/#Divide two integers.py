092616eb06c0f62d5db85569b334f38a61de8270
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2021-12-01 13:28:13 +0530

    Bits

--- Day 12/#Divide two integers.py
+++ Day 12/#Divide two integers.py
@@ -1,3 +1,4 @@
+
 class Solution(object):
     def divide(self, dividend, divisor):
         
@@ -8,22 +9,18 @@ class Solution(object):
         sign = (-1 if((dividend < 0) ^
                       (divisor < 0)) else 1);
 
-        # remove sign of operands
-        dividend = abs(dividend);
-        divisor = abs(divisor);
-
-        # Initialize
-        # the quotient
-        quotient = 0;
-        temp = 0;
-
-        # test down from the highest
-        # bit and accumulate the
-        # tentative value for valid bit
-        for i in range(31, -1, -1):
-            if (temp + (divisor << i) <= dividend):
-                temp += divisor << i;
-                quotient |= 1 << i;
-
-        return sign * quotient;
+        
+        a = abs(dividend);
+        b = abs(divisor);
+        res = 0
+        
+        while(a-b>=0):
+            x = 0 # 2^0 = 1
+            
+            while(a-(b<<1<<x) >= 0):
+                x += 1
+            
+            res += 1<<x
+            a -= b<<x
+        return res*sign
 
